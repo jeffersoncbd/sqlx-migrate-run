5e3e795621cfe87a6f936222eb07e302320d85fc
Jefferson Carlos <jefferson.diogo@trescon.com.br>
2025-03-03 14:59:01 -0300

    utiliza minio para cache

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,15 @@ set -e
 
 sqlx database create && echo "Banco de dados criado com sucesso."
 sqlx migrate run && echo "Migrations aplicadas com sucesso."
+cargo sqlx prepare && echo "Pré-compilação de queries realizada com sucesso."
+
+echo "Compactando pasta .sqlx para upload..."
+tar -czf /tmp/sqlx_cache.tar.gz .sqlx
+
+echo "Enviando arquivo .sqlx para o MinIO..."
+curl -X PUT -T /tmp/sqlx_cache.tar.gz "$MINIO_URL/$MINIO_BUCKET/$PROJECT_NAME/sqlx_cache.tar.gz" \
+  -H "Content-Type: application/octet-stream" \
+  -H "x-amz-acl: private" \
+  -u "$MINIO_ACCESS_KEY:$MINIO_SECRET_ACCESS_KEY"
+
+echo "Upload da pasta .sqlx para o MinIO concluído com sucesso."
