ff09b95a77fa1fcfa1dd3590247b65a7672546bd
Jefferson Carlos <31220819+jeffersoncbd@users.noreply.github.com>
2025-03-03 13:43:00 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,3 +3,7 @@ set -e
 
 sqlx database create && echo "Banco de dados criado com sucesso."
 sqlx migrate run && echo "Migrations aplicadas com sucesso."
+cargo sqlx prepare && echo "Informações do banco geradas para o SQLx"
+git add .
+git commit -m "sqlx prepare"
+git push
